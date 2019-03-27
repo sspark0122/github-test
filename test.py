833e898c838ab8f84e8a647aa3144f88bb0c63dc
Sung <sung@dhcp-nyc-226.redrover.cornell.edu>
2019-03-27 14:31:25 -0400

    Update test.py

--- test.py
+++ test.py
@@ -3,9 +3,13 @@ a = 1
 b = 2
 c = 3
 <<<<<<< HEAD
+<<<<<<< HEAD
 d = 4
 e = 5
 print(a+b+c+d+e)
+=======
+print(a+b+c)
+>>>>>>> parent of b42b085... Add a value of d.
 =======
 print(a+b+c)
 >>>>>>> parent of b42b085... Add a value of d.
