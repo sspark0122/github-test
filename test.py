78c8745843fcfe309c396438a19d788d9515010c
sspark0122 <sp2528@cornell.edu>
2019-03-27 15:04:43 -0400

    Update test.py

--- test.py
+++ test.py
@@ -2,18 +2,4 @@
 a = 1
 b = 2
 c = 3
-<<<<<<< HEAD
-<<<<<<< HEAD
-d = 4
-<<<<<<< HEAD
-e = 5
-print(a+b+c+d+e)
-=======
 print(a+b+c)
->>>>>>> parent of b42b085... Add a value of d.
-=======
-print(a+b+c)
->>>>>>> parent of b42b085... Add a value of d.
-=======
-print(a+b+c+d)
->>>>>>> parent of d585683... Add a value of e.
