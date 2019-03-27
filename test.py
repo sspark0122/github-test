75aab4374b06a33575a6ce2952e0e05b492a26f7
sspark0122 <sp2528@cornell.edu>
2019-03-27 15:00:05 -0400

    Update test.py

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ c = 3
 <<<<<<< HEAD
 <<<<<<< HEAD
 d = 4
+<<<<<<< HEAD
 e = 5
 print(a+b+c+d+e)
 =======
@@ -13,3 +14,6 @@ print(a+b+c)
 =======
 print(a+b+c)
 >>>>>>> parent of b42b085... Add a value of d.
+=======
+print(a+b+c+d)
+>>>>>>> parent of d585683... Add a value of e.
